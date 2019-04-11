a0d9cb767a319e147a3a05849e3a95102144658a
azorus-98 <juan219810@hotmail.com>
2019-04-11 12:37:39 +0200

    nociones de programacion tanto en js como en css

---
+++ 0001_comienzos_de_js/js/app.js
@@ -0,0 +1,7 @@
+let x =12
+let y =3
+let suma
+suna -x+y
+console.log(suma)/* esto sirve para ejecutar la cosa en parentesis */
+console.dir(suma)/* esto sirve para ejecutar la cosa en parentesis */
+/* para */
