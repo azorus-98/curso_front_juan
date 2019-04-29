74410510666757e0623f184772362ea1a165e902
azorus-98 <juan219810@hotmail.com>
2019-04-29 13:57:53 +0200

    lo

---
+++ js/21_objetos.js
@@ -0,0 +1,12 @@
+let user1={
+    nombre: 'pepe',
+    edad: 24,
+    saludar: function () {
+        console.log(`hola,soy ${this.nombre}`)
+    }
+}
+
+let user2={
+    nombre: 'rosa',
+    edad:27,
+}
